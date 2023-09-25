6260c56e2e55ba5a754a47ddc2937432d29615aa
guoyi <13264709273@100fen.cn>
2023-09-25 11:47:49 +0800

    feat:响应式原理

--- catelog/js/index.js
+++ catelog/js/index.js
@@ -1,7 +1,22 @@
 /* const a=[];
 a[10]=6;
 console.log(a); */
-const arr=[1,3]
-arr.push({c:6})
-console.log(arr);
-console.log(arr instanceof Array);
+// const arr=[1,3]
+// arr.push({c:6})
+// console.log(arr);
+// console.log(arr instanceof Array);
+const u={
+  binds:[6,7]
+}
+const t={
+  ...u,
+  binds:[8,9]
+}
+console.log(t);
+let str = ''
+async function fill(){
+  return new Promise(resolve => setTimeout(resolve('666'), 10))
+};
+setTimeout(()=>{
+  console.log(fill());
+},2000)
